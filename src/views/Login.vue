7fefae0ab282ad1ef7d1d55b8a3ad38d8858c537
KidDarknezz <darkslave@live.com>
2019-10-15 15:12:44 -0500

    quick syntax update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,11 +8,11 @@
 		<b-row style="margin-top: 50px">
 			<b-col cols="12">
 				<label>Correo:</label>
-				<input type="email" class="form-control" placeholder="test@test.com" v-model="user" v-on:keyup.enter="login">
+				<input type="email" class="form-control" placeholder="test@test.com" v-model="user" @keyup.enter="login">
 			</b-col>
 			<b-col cols="12" style="margin-top: 20px;">
 				<label>Contraseña:</label>
-				<input type="password" class="form-control" placeholder="password" v-model="password" v-on:keyup.enter="login">
+				<input type="password" class="form-control" placeholder="password" v-model="password" @keyup.enter="login">
 			</b-col>
 		</b-row>
 		<br>
