00df43a746056ea2a65c30e9143208c8541c9893
Diego Rodriguez <lexelEZ@gmail.com>
2019-11-08 12:30:58 -0500

    initial setup for firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const db = admin.firestore()
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send('Hello from Firebase!')
+})
