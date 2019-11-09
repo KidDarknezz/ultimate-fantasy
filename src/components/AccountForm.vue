e5984d06098a52d6552d0db15992af12c17eab4c
Diego Rodriguez <lexelEZ@gmail.com>
2019-11-09 00:12:49 -0500

    Implemented firebase authentification for newusers

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -1,23 +1,55 @@
 <template>
-	<b-row>
-		<b-col cols="12">
-			<label>Nombre:</label>
-			<input type="text" class="form-control" placeholder="John Doe" v-model="$store.state.newUser.name">
-			<label>Email:</label>
-			<input type="email" class="form-control" placeholder="john.doe@test.com" v-model="$store.state.newUser.email">
-			<label>Contraseña</label>
-			<input type="password" class="form-control" placeholder="password" v-model="$store.state.newUser.password">
-			<label>Nombra tu equipo fantasy</label>
-			<input type="text" class="form-control" v-model="$store.state.newUser.teamName">
-			<br>
-		</b-col>
-	</b-row>
+    <b-row>
+        <b-col cols="12">
+            <label>Nombre:</label>
+            <input type="text" class="form-control" placeholder="John Doe" v-model="name" />
+            <label>Email:</label>
+            <input
+                type="email"
+                class="form-control"
+                placeholder="john.doe@test.com"
+                v-model="email"
+            />
+            <label>Contraseña</label>
+            <input type="password" class="form-control" placeholder="password" v-model="password" />
+            <label>Nombra tu equipo fantasy</label>
+            <input type="text" class="form-control" v-model="teamName" />
+            <br />
+            <button @click="validateForm" class="btn btn-success">Seleccionar roster</button>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            teamName: '',
+        }
+    },
+    methods: {
+        validateForm() {
+            if (
+                this.name == '' ||
+                this.email == '' ||
+                this.password == '' ||
+                this.teamName == ''
+            ) {
+                alert('Porfavor llena todos los campos')
+            }
+            this.$emit('formSuccess', {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+                teamName: this.teamName,
+            })
+        },
+    },
+}
 </script>
 
 <style>
-
 </style>
--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,56 +1,66 @@
 <template>
-	<b-container>
-		<b-row class="heading">
-			<b-col cols="12">
-				<h3 v-if="!step">Crear cuenta</h3>
-				<h3 v-if="step">Selecciona tu roster</h3>
-			</b-col>
-		</b-row>
-		<account-form v-if="!step" class="content"></account-form>
-		<button @click="validateForm" class="btn btn-success" v-if="!step">Seleccionar roster</button>
-		<pick-roster v-if="step" class="content"></pick-roster>
-	</b-container>
+    <b-container>
+        <b-row class="heading">
+            <b-col cols="12">
+                <h3 v-if="!step">Crear cuenta</h3>
+                <h3 v-if="step">Selecciona tu roster</h3>
+            </b-col>
+        </b-row>
+        <account-form v-if="!step" class="content" @formSuccess="createUserAccount"></account-form>
+        <pick-roster v-if="step" class="content"></pick-roster>
+    </b-container>
 </template>
 
 <script>
-	import AccountForm from '@/components/AccountForm.vue'
-	import PickRoster from '@/components/PickRoster.vue'
-	export default {
-		data: function() {
-			return {
-				step: false,
-				originTeams: []
-			}
-		},
-		methods: {
-			getAllOriginTeams: function() {
-				this.$http.get(`https://ultimate-fantasy-fe04f.firebaseio.com/teams.json`)
-					.then(response => {
-						return response.json()
-					})
-					.then(data => {
-						const resultArray = []
-						for (let key in data) {
-							resultArray.push(data[key])
-						}
-						this.$store.state.allOriginTeams = resultArray
-					})
-			},
-			validateForm: function() {
-				let newUser = this.$store.state.newUser
-				if(newUser.name == "" || newUser.email == "" || newUser.password == "" || newUser.teamName == "") {
-					alert("Porfavor llena todos los campos")
-				} else {
-					this.step = !this.step
-				}
-			}
-		},
-		beforeMount: function() {
-			this.getAllOriginTeams()
-		},
-		components: {
-			'account-form': AccountForm,
-			'pick-roster': PickRoster
-		}
-	}
+import AccountForm from '@/components/AccountForm.vue'
+import PickRoster from '@/components/PickRoster.vue'
+import firebase from 'firebase/app'
+
+export default {
+    components: {
+        'account-form': AccountForm,
+        'pick-roster': PickRoster,
+    },
+    data: function() {
+        return {
+            step: false,
+            originTeams: [],
+        }
+    },
+    methods: {
+        createUserAccount(user) {
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(user.email, user.password)
+                .then(alert('Account Created Successfully!'))
+                .then(() => {
+                    this.step = true
+                })
+                .catch(error => {
+                    // Handle Errors here.
+                    console.log(error)
+                    var errorCode = error.code
+                    var errorMessage = error.message
+                    // ...
+                })
+        },
+        getAllOriginTeams() {
+            this.$http
+                .get(`https://ultimate-fantasy-fe04f.firebaseio.com/teams.json`)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    const resultArray = []
+                    for (let key in data) {
+                        resultArray.push(data[key])
+                    }
+                    this.$store.state.allOriginTeams = resultArray
+                })
+        },
+    },
+    beforeMount() {
+        this.getAllOriginTeams()
+    },
+}
 </script>
