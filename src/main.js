48bd44192313e8e231e837d11b7d652a67e78f51
Diego Rodriguez <lexelEZ@gmail.com>
2019-11-09 00:12:49 -0500

    Implemented firebase authentification for newusers

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { store } from './store'
+import {store} from './store'
 import BootstrapVue from 'bootstrap-vue'
 import VueResource from 'vue-resource'
+
+import firebase from 'firebase/app'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './styles.css'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD0-NVXOZ31yc_d-vWTS70SNo91sxroJnI',
+    authDomain: 'ultimate-fantasy-fe04f.firebaseapp.com',
+    databaseURL: 'https://ultimate-fantasy-fe04f.firebaseio.com',
+    projectId: 'ultimate-fantasy-fe04f',
+    storageBucket: 'ultimate-fantasy-fe04f.appspot.com',
+    messagingSenderId: '759726116031',
+    appId: '1:759726116031:web:b816abb7a09c59efa2ca0a',
+    measurementId: 'G-0J8P5WE07K',
+}
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.use(BootstrapVue)
 Vue.use(VueResource)
@@ -14,7 +30,7 @@ Vue.use(VueResource)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
--- src/views/CreateInfo.vue
+++ src/views/CreateInfo.vue
@@ -1,73 +1,83 @@
 <template>
-	<b-container>
-		<b-row>
-			<b-col cols="12">
-				<h3>Importar Data</h3>
-			</b-col>
-		</b-row>
-		<b-row>
-			<b-col cols="12" style="margin-top: 20px;">
-				<textarea class="form-control" rows="12" v-model="importedData"></textarea>
-			</b-col>
-			<b-col cols="12">
-				<button class="btn btn-info" style="margin-top: 20px;" @click="importData">IMPORTAR</button>
-			</b-col>
-		</b-row>
-	</b-container>
+    <b-container>
+        <b-row>
+            <b-col cols="12">
+                <h3>Importar Data</h3>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12" style="margin-top: 20px;">
+                <textarea class="form-control" rows="12" v-model="importedData"></textarea>
+            </b-col>
+            <b-col cols="12">
+                <button class="btn btn-info" style="margin-top: 20px;" @click="importData">IMPORTAR</button>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-	export default {
-		data: function() {
-			return{
-				importedData: '',
-				patchScore: {
-					score: 0
-				}
-			}
-		},
-		methods: {
-			importData: function() {
-				let jsonImportedData = JSON.parse(this.importedData)
-				for(let j in jsonImportedData) {
-					for (let i in this.$store.state.allPlayers) {
-						if (this.$store.state.allPlayers[i].playerId == jsonImportedData[j].playerId) {
-							this.patchScore.score = this.$store.state.allPlayers[i].score + jsonImportedData[j].score
-						}
-					}
-					this.patchNewScore(jsonImportedData[j].playerId)
-				}
-				
-			},
-			getPlayers: function() {
-				this.$http.get(`https://ultimate-fantasy-fe04f.firebaseio.com/players.json`)
-					.then(response => {
-						return response.json()
-					})
-					.then(data => {
-						const resultArray = []
-						for (let key in data) {
-							resultArray.push(data[key])
-						}
-						this.$store.state.allPlayers = resultArray
-					})
-			},
-			patchNewScore: function(playerId) {
-				this.$http.patch(`https://ultimate-fantasy-fe04f.firebaseio.com/players/${playerId}.json`, this.patchScore)
-				.then(response => {
-					console.log(response)
-				})
-				.then(err => {
-					console.log(err)
-				})
-			}
-		},
-		beforeMount: function() {
-			this.getPlayers()
-		}
-	}
+export default {
+    data: function() {
+        return {
+            importedData: '',
+            patchScore: {
+                score: 0,
+            },
+        }
+    },
+    methods: {
+        importData: function() {
+            let jsonImportedData = JSON.parse(this.importedData)
+            for (let j in jsonImportedData) {
+                for (let i in this.$store.state.allPlayers) {
+                    if (
+                        this.$store.state.allPlayers[i].playerId ==
+                        jsonImportedData[j].playerId
+                    ) {
+                        this.patchScore.score =
+                            this.$store.state.allPlayers[i].score +
+                            jsonImportedData[j].score
+                    }
+                }
+                this.patchNewScore(jsonImportedData[j].playerId)
+            }
+        },
+        getPlayers: function() {
+            this.$http
+                .get(
+                    `https://ultimate-fantasy-fe04f.firebaseio.com/players.json`
+                )
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    const resultArray = []
+                    for (let key in data) {
+                        resultArray.push(data[key])
+                    }
+                    this.$store.state.allPlayers = resultArray
+                })
+        },
+        patchNewScore: function(playerId) {
+            this.$http
+                .patch(
+                    `https://ultimate-fantasy-fe04f.firebaseio.com/players/${playerId}.json`,
+                    this.patchScore
+                )
+                .then(response => {
+                    console.log(response)
+                })
+                .then(err => {
+                    console.log(err)
+                })
+        },
+    },
+    beforeMount: function() {
+        this.getPlayers()
+    },
+}
 </script>
 
 <style>
-
 </style>
