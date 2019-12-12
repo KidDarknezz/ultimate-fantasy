05e1cfc5ebfee0fdd687c2f18ae1ff28dc7b0f23
LeXeL <diego.r2892@gmail.com>
2019-12-12 17:33:25 -0500

    General Bug fixes

--- src/components/PickRoster.vue
+++ src/components/PickRoster.vue
@@ -1,70 +1,110 @@
 <template>
-	<b-row>
-		<b-col cols="12">
-			<p style="text-align: center;">
-				Selecciona 3 jugadores del equipo 
-				<strong>{{ this.$store.state.allOriginTeams[teamStep].name }}</strong>
-			</p>
-			<!-- <p>{{ this.$store.state.allOriginTeams[teamStep].name }}</p> -->
-			<b-img :src="logos[teamStep]" center fluid style="width: 25%; margin-bottom: 20px;"></b-img>
-			<b-form-group>
-				<b-form-checkbox-group id="checkbox-group-2" v-model="selected" name="flavour-2" stacked>
-					<b-form-checkbox style="margin-bottom: 8px;" :value="player.playerId" v-for="player in options" v-if="player.originTeam == teamStep">{{ player.name }}</b-form-checkbox>
-				</b-form-checkbox-group>
-			</b-form-group>
-			<button style="margin-bottom: 20px" class="btn btn-success" @click="checkPicks" v-if="teamStep != (this.$store.state.allOriginTeams.length - 1)">Siguiente</button>
-			<button style="margin-bottom: 20px" class="btn btn-success" @click="createAccount" v-if="teamStep == (this.$store.state.allOriginTeams.length - 1)">Finalizar</button>
-		</b-col>
-	</b-row>
+    <b-row>
+        <b-col cols="12">
+            <p style="text-align: center;">
+                Selecciona 3 jugadores del equipo
+                <strong>{{
+                    this.$store.state.allOriginTeams[teamStep].name
+                }}</strong>
+            </p>
+            <!-- <p>{{ this.$store.state.allOriginTeams[teamStep].name }}</p> -->
+            <b-img
+                :src="logos[teamStep]"
+                center
+                fluid
+                style="width: 25%; margin-bottom: 20px;"
+            ></b-img>
+            <b-form-group>
+                <b-form-checkbox-group
+                    id="checkbox-group-2"
+                    v-model="selected"
+                    name="flavour-2"
+                    stacked
+                >
+                    <b-form-checkbox
+                        style="margin-bottom: 8px;"
+                        :value="player.playerId"
+                        v-for="player in options"
+                        v-if="player.originTeam == teamStep"
+                        :key="player"
+                        >{{ player.name }}</b-form-checkbox
+                    >
+                </b-form-checkbox-group>
+            </b-form-group>
+            <button
+                style="margin-bottom: 20px"
+                class="btn btn-success"
+                @click="checkPicks"
+                v-if="teamStep != this.$store.state.allOriginTeams.length - 1"
+            >
+                Siguiente
+            </button>
+            <button
+                style="margin-bottom: 20px"
+                class="btn btn-success"
+                @click="createAccount"
+                v-if="teamStep == this.$store.state.allOriginTeams.length - 1"
+            >
+                Finalizar
+            </button>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-  export default {
+export default {
     data() {
-		return {
-			selected: [],
-			options: this.$store.state.allPlayers,
-			teamStep: 0,
-			rosterCounter: 3,
-			logos: [
-				'https://ultiranks.com/wp-content/uploads/2019/09/Raiders_logo.png',
-				'https://ultiranks.com/wp-content/uploads/2019/09/Owls_logo.png',
-				'https://ultiranks.com/wp-content/uploads/2019/09/Dragons_logo.png',
-				'https://ultiranks.com/wp-content/uploads/2019/09/Eagles_logo.png'
-			]
-		}
-	},
-	methods: {
-		checkPicks: function() {
-			if (this.teamStep < this.$store.state.allOriginTeams.length) {
-				if ((this.selected.length % this.rosterCounter) == 0 && this.selected.length > 0) {
-					this.teamStep++
-					this.rosterCounter += 3
-					return true
-				} else {
-					alert("Seleccione 3")
-				}	
-			} 
-		},
-		createAccount: function() {
-			this.$store.state.newUser.roster = (this.selected)
-			let sendData = this.checkPicks()
-			if(sendData) {
-				this.$http.post(`https://ultimate-fantasy-fe04f.firebaseio.com/users.json`, this.$store.state.newUser)
-					.then(response => {
-						console.log(response)
-						alert("Tu equipo se ha creado con exito, puedes iniciar sesion")
-						this.$router.push('/')
-					})
-					.then(err => {
-						console.log(err)
-					})
-			}
-		}
-	}
-  }
+        return {
+            selected: [],
+            options: this.$store.state.allPlayers,
+            teamStep: 0,
+            rosterCounter: 3,
+            logos: [
+                'https://ultiranks.com/wp-content/uploads/2019/09/Raiders_logo.png',
+                'https://ultiranks.com/wp-content/uploads/2019/09/Owls_logo.png',
+                'https://ultiranks.com/wp-content/uploads/2019/09/Dragons_logo.png',
+                'https://ultiranks.com/wp-content/uploads/2019/09/Eagles_logo.png',
+            ],
+        }
+    },
+    methods: {
+        checkPicks: function() {
+            if (this.teamStep < this.$store.state.allOriginTeams.length) {
+                if (
+                    this.selected.length % this.rosterCounter == 0 &&
+                    this.selected.length > 0
+                ) {
+                    this.teamStep++
+                    this.rosterCounter += 3
+                    return true
+                } else {
+                    alert('Seleccione 3')
+                }
+            }
+        },
+        createAccount: function() {
+            this.$store.state.newUser.roster = this.selected
+            let sendData = this.checkPicks()
+            if (sendData) {
+                this.$http
+                    .post(
+                        `https://ultimate-fantasy-fe04f.firebaseio.com/users.json`,
+                        this.$store.state.newUser
+                    )
+                    .then(response => {
+                        console.log(response)
+                        alert(
+                            'Tu equipo se ha creado con exito, puedes iniciar sesion'
+                        )
+                        this.$router.push('/')
+                    })
+                    .then(err => {
+                        console.log(err)
+                    })
+            }
+        },
+    },
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/TeamRoster.vue
+++ src/views/TeamRoster.vue
@@ -1,73 +1,81 @@
 <template>
-	<b-container>
-		<b-row class="heading">
-			<b-col cols="12">
-				<router-link to="/home">B</router-link>
-			</b-col>
-			<b-col cols="12">
-				<h3>{{ teamInfo.teamName }}</h3>
-				<p style="text-align: center;">{{ teamInfo.name }}</p>
-			</b-col>
-		</b-row>
-		<b-row class="content" v-for="roster in teamRoster">
-			<b-col cols="3">
-				<p style="margin-bottom: 0">{{ roster.originTeam | assignTeam }}</p>
-			</b-col>
-			<b-col cols="6">
-				<p style="margin-bottom: 0">{{ roster.name }}</p>
-			</b-col>
-			<b-col cols="2">
-				<p style="margin-bottom: 0">{{ roster.score }}</p>
-			</b-col>
-			<hr>
-		</b-row>
-	</b-container>
+    <b-container>
+        <b-row class="heading">
+            <b-col cols="12">
+                <router-link to="/home">B</router-link>
+            </b-col>
+            <b-col cols="12">
+                <h3>{{ teamInfo.teamName }}</h3>
+                <p style="text-align: center;">{{ teamInfo.name }}</p>
+            </b-col>
+        </b-row>
+        <b-row class="content" v-for="roster in teamRoster" :key="roster">
+            <b-col cols="3">
+                <p style="margin-bottom: 0">
+                    {{ roster.originTeam | assignTeam }}
+                </p>
+            </b-col>
+            <b-col cols="6">
+                <p style="margin-bottom: 0">{{ roster.name }}</p>
+            </b-col>
+            <b-col cols="2">
+                <p style="margin-bottom: 0">{{ roster.score }}</p>
+            </b-col>
+            <hr />
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-	export default {
-		data: function() {
-			return {
-				teamInfo: {},
-				teamRoster: []
-			}
-		},
-		methods: {
-			getTeam: function() {
-				this.$http.get(`https://ultimate-fantasy-fe04f.firebaseio.com/users/${this.$route.params.id}.json`)
-					.then(response => {
-						this.teamInfo = response.data
-						this.pushTeamRoster()
-					})
-			},
-			pushTeamRoster: function() {
-				for(let i in this.teamInfo.roster) {
-					for(let j in this.$store.state.allPlayers) {
-						if (this.teamInfo.roster[i] == this.$store.state.allPlayers[j].playerId) {
-							this.teamRoster.push(this.$store.state.allPlayers[j])
-						}
-					}
-				}
-			}
-		},
-		beforeMount: function() {
-			this.getTeam()
-		},
-		filters: {
-			assignTeam: function(value) {
-				if (value == 0) {
-					return `Raiders`
-				}
-				if (value == 1) {
-					return `Owls`
-				}
-				if (value == 2) {
-					return `Dragons`
-				}
-				if (value == 3) {
-					return `Eagles`
-				}
-			}
-		}
-	}
+export default {
+    data: function() {
+        return {
+            teamInfo: {},
+            teamRoster: [],
+        }
+    },
+    methods: {
+        getTeam: function() {
+            this.$http
+                .get(
+                    `https://ultimate-fantasy-fe04f.firebaseio.com/users/${this.$route.params.id}.json`
+                )
+                .then(response => {
+                    this.teamInfo = response.data
+                    this.pushTeamRoster()
+                })
+        },
+        pushTeamRoster: function() {
+            for (let i in this.teamInfo.roster) {
+                for (let j in this.$store.state.allPlayers) {
+                    if (
+                        this.teamInfo.roster[i] ==
+                        this.$store.state.allPlayers[j].playerId
+                    ) {
+                        this.teamRoster.push(this.$store.state.allPlayers[j])
+                    }
+                }
+            }
+        },
+    },
+    beforeMount: function() {
+        this.getTeam()
+    },
+    filters: {
+        assignTeam: function(value) {
+            if (value == 0) {
+                return `Raiders`
+            }
+            if (value == 1) {
+                return `Owls`
+            }
+            if (value == 2) {
+                return `Dragons`
+            }
+            if (value == 3) {
+                return `Eagles`
+            }
+        },
+    },
+}
 </script>
