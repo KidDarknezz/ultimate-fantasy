809b797e597f75cf72cb1e2436283feb0b90ad56
LeXeL <diego.r2892@gmail.com>
2019-12-12 17:33:25 -0500

    General Bug fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,83 +1,106 @@
 <template>
-	<b-container>
-		<b-row class="heading">
-			<b-col cols="12">
-				<h3>{{ loginUser.teamName }}</h3>
-			</b-col>
-			<b-col cols="12">
-				<h4 class="score"><span>Puntos</span><br>{{ fantasyScore }}</h4>
-			</b-col>
-		</b-row>
-		<b-row class="content">
-			<b-col cols="12">
-				<p style="font-weight: 700;">Ranking</p>
-			</b-col>			
-			<b-col cols="12" class="teams-list" style="margin-bottom: 20px;">
-				<b-list-group>
-					<router-link :to="`/team/${team.id}`" tag="b-list-group-item" v-for="(team, index) in this.$store.state.allUsers" >
-						<span class="ranking-name">{{ index + 1 }}. {{ team.teamName }}</span> - 
-						<span class="ranking-score">{{ team.score }}</span> Pts.<br>
-						<span class="ranking-owner">{{ team.name }}</span>
-					</router-link>
-				</b-list-group>
-			</b-col>
-		</b-row>
-	</b-container>
+    <b-container>
+        <b-row class="heading">
+            <b-col cols="12">
+                <h3>{{ loginUser.teamName }}</h3>
+            </b-col>
+            <b-col cols="12">
+                <h4 class="score">
+                    <span>Puntos</span>
+                    <br />
+                    {{ fantasyScore }}
+                </h4>
+            </b-col>
+        </b-row>
+        <b-row class="content">
+            <b-col cols="12">
+                <p style="font-weight: 700;">Ranking</p>
+            </b-col>
+            <b-col cols="12" class="teams-list" style="margin-bottom: 20px;">
+                <b-list-group>
+                    <router-link
+                        :to="`/team/${team.id}`"
+                        tag="b-list-group-item"
+                        v-for="(team, index) in this.$store.state.allUsers"
+                        :key="index"
+                    >
+                        <span class="ranking-name">{{ index + 1 }}. {{ team.teamName }}</span>
+                        -
+                        <span class="ranking-score">{{ team.score }}</span>
+                        Pts.
+                        <br />
+                        <span class="ranking-owner">{{ team.name }}</span>
+                    </router-link>
+                </b-list-group>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-	export default {
-		data: function() {
-			return {
-				loginUser: {},
-				fantasyScore: 0,
-			}
-		},
-		methods: {
-			getUser: function() {
-				this.$http.get(`https://ultimate-fantasy-fe04f.firebaseio.com/users/${localStorage.userId}.json`)
-					.then(response => {
-						return response.json()
-					})
-					.then(data => {
-						this.loginUser = data
-						this.calculateScore()
-					})
-			},
-			calculateScore: function() {
-				for (let i in this.loginUser.roster) {
-					for (let j in this.$store.state.allPlayers) {
-						if (this.loginUser.roster[i] == this.$store.state.allPlayers[j].playerId) {
-							this.fantasyScore += this.$store.state.allPlayers[j].score
-						}
-					}
-				}
-			},
-			calculateRanking: function() {
-				for(let i in this.$store.state.allUsers) {
-					var score = 0
-					for(let j in this.$store.state.allUsers[i].roster) {
-						for(let k in this.$store.state.allPlayers) {
-							if (this.$store.state.allUsers[i].roster[j] == this.$store.state.allPlayers[k].playerId) {
-								score += this.$store.state.allPlayers[k].score
-							}
-						}
-					}
-					this.$store.state.allUsers[i].score = score
-				}
-				this.sortRanking()
-			},
-			sortRanking: function() {
-				this.$store.state.allUsers.sort((a, b) => (a.score > b.score) ? -1 : 1)
-			}
-		},
-		beforeMount: function() {
-			this.getUser()
-			this.calculateRanking()
-		}
-	}
+export default {
+    data() {
+        return {
+            loginUser: {},
+            fantasyScore: 0,
+        }
+    },
+    methods: {
+        getUser() {
+            this.$http
+                .get(
+                    `https://ultimate-fantasy-fe04f.firebaseio.com/users/${localStorage.userId}.json`
+                )
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    this.loginUser = data
+                    this.calculateScore()
+                })
+        },
+        calculateScore() {
+            for (let i in this.loginUser.roster) {
+                for (let j in this.$store.state.allPlayers) {
+                    if (
+                        this.loginUser.roster[i] ==
+                        this.$store.state.allPlayers[j].playerId
+                    ) {
+                        this.fantasyScore += this.$store.state.allPlayers[
+                            j
+                        ].score
+                    }
+                }
+            }
+        },
+        calculateRanking() {
+            for (let i in this.$store.state.allUsers) {
+                var score = 0
+                for (let j in this.$store.state.allUsers[i].roster) {
+                    for (let k in this.$store.state.allPlayers) {
+                        if (
+                            this.$store.state.allUsers[i].roster[j] ==
+                            this.$store.state.allPlayers[k].playerId
+                        ) {
+                            score += this.$store.state.allPlayers[k].score
+                        }
+                    }
+                }
+                this.$store.state.allUsers[i].score = score
+            }
+            this.sortRanking()
+        },
+        sortRanking() {
+            this.$store.state.allUsers.sort((a, b) =>
+                a.score > b.score ? -1 : 1
+            )
+        },
+    },
+    beforeMount() {
+        this.getUser()
+        this.calculateRanking()
+    },
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
