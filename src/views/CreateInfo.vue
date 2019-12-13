48e8f5cfa3b7301165673d82cecb26c125e4adbd
LeXeL <diego.r2892@gmail.com>
2019-12-13 18:02:01 -0500

    Updated this view to manage new events

--- src/views/CreateInfo.vue
+++ src/views/CreateInfo.vue
@@ -7,74 +7,79 @@
         </b-row>
         <b-row>
             <b-col cols="12" style="margin-top: 20px;">
+                <label>Nombre del Evento:</label>
+                <b-form-input v-model="eventName"></b-form-input>
+            </b-col>
+            <b-col cols="12" style="margin-top: 20px;">
                 <textarea class="form-control" rows="12" v-model="importedData"></textarea>
             </b-col>
             <b-col cols="12">
                 <button class="btn btn-info" style="margin-top: 20px;" @click="importData">IMPORTAR</button>
             </b-col>
         </b-row>
+        <b-row v-if="log">
+            <h3>Log:</h3>
+            <h2>{{log}}</h2>
+        </b-row>
     </b-container>
 </template>
 
 <script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 export default {
-    data: function() {
+    data() {
         return {
             importedData: '',
+            eventName: '',
             patchScore: {
                 score: 0,
             },
+            log: '',
         }
     },
     methods: {
-        importData: function() {
+        importData() {
+            let Data = {}
             let jsonImportedData = JSON.parse(this.importedData)
-            for (let j in jsonImportedData) {
-                for (let i in this.$store.state.allPlayers) {
-                    if (
-                        this.$store.state.allPlayers[i].playerId ==
-                        jsonImportedData[j].playerId
-                    ) {
-                        this.patchScore.score =
-                            this.$store.state.allPlayers[i].score +
-                            jsonImportedData[j].score
+            jsonImportedData.forEach(player => {
+                var team = player['Equipo']
+                if (!Data.hasOwnProperty(team)) {
+                    Data[team] = {
+                        Jugadores: [
+                            {
+                                nombre: player.Nombre,
+                                nivel: player.Nivel,
+                                Asist: parseInt(player.Asist),
+                                Gol: parseInt(player.Goles),
+                                Def: parseInt(player.Def),
+                            },
+                        ],
                     }
+                } else {
+                    Data[team].Jugadores.push({
+                        nombre: player.Nombre,
+                        nivel: player.Nivel,
+                        Asist: parseInt(player.Asist),
+                        Gol: parseInt(player.Goles),
+                        Def: parseInt(player.Def),
+                    })
                 }
-                this.patchNewScore(jsonImportedData[j].playerId)
-            }
+            })
+            var db = firebase.firestore()
+            Object.keys(Data).forEach(equipo => {
+                db.collection(this.eventName)
+                    .doc(equipo)
+                    .set(Data[equipo])
+                    .then(() => {
+                        this.log += `[+]Updated Succesfully ${equipo}\n`
+                    })
+                    .catch(error => {
+                        this.log += `[*]Hubo un error: ${error}\n`
+                    })
+            })
         },
-        getPlayers: function() {
-            this.$http
-                .get(
-                    `https://ultimate-fantasy-fe04f.firebaseio.com/players.json`
-                )
-                .then(response => {
-                    return response.json()
-                })
-                .then(data => {
-                    const resultArray = []
-                    for (let key in data) {
-                        resultArray.push(data[key])
-                    }
-                    this.$store.state.allPlayers = resultArray
-                })
-        },
-        patchNewScore: function(playerId) {
-            this.$http
-                .patch(
-                    `https://ultimate-fantasy-fe04f.firebaseio.com/players/${playerId}.json`,
-                    this.patchScore
-                )
-                .then(response => {
-                    console.log(response)
-                })
-                .then(err => {
-                    console.log(err)
-                })
-        },
-    },
-    beforeMount: function() {
-        this.getPlayers()
     },
 }
 </script>
