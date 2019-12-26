f6225f702ed72f0f23b6da07ae81b15013502b0d
KidDarknezz <darkslave@live.com>
2019-12-25 23:56:10 -0500

    Login minor fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,7 +34,7 @@
                     <input
                         type="password"
                         class="form-control"
-                        placeholder="password"
+                        placeholder="**********"
                         v-model="password"
                         @keyup.enter="login"
                     />
