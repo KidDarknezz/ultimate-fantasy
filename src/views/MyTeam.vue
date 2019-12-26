e23fb43b68f710d077c022e8c56dcea63c025c59
KidDarknezz <darkslave@live.com>
2019-12-25 23:51:16 -0500

    My team info view

---
+++ src/views/MyTeam.vue
@@ -0,0 +1,134 @@
+<template>
+	<div>
+		<!-- HEADER -->
+        <div class="row-fluid header">
+            <img src="@/assets/user-placeholder.jpg" class="img-fluid" style="float: left; margin-right: 15px;">
+            <h2 style="font-size: 25px; padding-top: 15px">Alejandro Millan</h2>
+        </div>
+        <!-- END HEADER -->
+
+        <!-- SUB HEADER -->
+        <b-container style="margin: 0; padding-top: 10px; padding-bottom: 10px; background-color: #000;">
+            <b-row>
+                <b-col cols="2">
+                    <img src="@/assets/cds.png" style="padding-top: 10px;" class="img-fluid">
+                </b-col>
+                <b-col cols="10">
+                    <h4>
+                        Liga de Verano
+                        <br>
+                        Ciudad del Saber 2020
+                    </h4>
+                </b-col>
+            </b-row>
+        </b-container>
+        <!-- END SUBHEADER -->
+
+        <!-- ROSTER LIST -->
+        <b-container class="dem-fantasy-roster">
+        	<p>Capitan</p>
+        	<div class="dem-fantasy-roster-player">
+        		<b-row>
+        			<b-col cols="3">
+        				<img src="@/assets/user-placeholder.jpg" class="img-fluid">
+        			</b-col>
+        			<b-col cols="7">
+        				<span>Nombre Jugador</span>
+        				<br>
+        				<span>Equipo</span>
+        			</b-col>
+        			<b-col cols="2">
+        				<span class="dem-fantasy-roster-score">500</span>
+        			</b-col>
+        		</b-row>
+        		<b-row class="dem-fantasy-player-stats">
+        			<b-col cols="4">
+        				<span>Goles</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Asistencias</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Defensas</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        		</b-row>
+        	</div>
+        	<br>
+        	<p>Cortadores</p>
+        	<div class="dem-fantasy-roster-player">
+        		<b-row>
+        			<b-col cols="3">
+        				<img src="@/assets/user-placeholder.jpg" class="img-fluid">
+        			</b-col>
+        			<b-col cols="7">
+        				<span>Nombre Jugador</span>
+        				<br>
+        				<span>Equipo</span>
+        			</b-col>
+        			<b-col cols="2">
+        				<span class="dem-fantasy-roster-score">500</span>
+        			</b-col>
+        		</b-row>
+        		<b-row class="dem-fantasy-player-stats">
+        			<b-col cols="4">
+        				<span>Goles</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Asistencias</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Defensas</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        		</b-row>
+        	</div>
+        	<br>
+        	<p>Handlers</p>
+        	<div class="dem-fantasy-roster-player">
+        		<b-row>
+        			<b-col cols="3">
+        				<img src="@/assets/user-placeholder.jpg" class="img-fluid">
+        			</b-col>
+        			<b-col cols="7">
+        				<span>Nombre Jugador</span>
+        				<br>
+        				<span>Equipo</span>
+        			</b-col>
+        			<b-col cols="2">
+        				<span class="dem-fantasy-roster-score">500</span>
+        			</b-col>
+        		</b-row>
+        		<b-row class="dem-fantasy-player-stats">
+        			<b-col cols="4">
+        				<span>Goles</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Asistencias</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        			<b-col cols="4">
+        				<span>Defensas</span>
+        				<br>
+        				<span class="dem-fantasy-roster-score">19</span>
+        			</b-col>
+        		</b-row>
+        	</div>
+        	<br>
+        </b-container>
+        <!-- END ROSTER LIST -->
+	</div>
+</template>
