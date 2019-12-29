ad1fc472f0a94b7e83f48d8e591fa6eea3ff7009
Diego Rodriguez <lexelEZ@gmail.com>
2019-12-29 00:56:36 -0500

    fixed login refresh bug

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,16 @@
         <router-view />
     </div>
 </template>
+<script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+export default {
+    async mounted() {
+        firebase.auth().onAuthStateChanged(async user => {
+            if (user) {
+                await this.$store.dispatch('setCurrentUser', user)
+            }
+        })
+    },
+}
+</script>
